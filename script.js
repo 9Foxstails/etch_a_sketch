7859637bd31cc228d13b1e90aa52a58f54a76957
9Foxstails <t-mozer@hotmail.com>
2025-04-09 12:25:03 +0300

    feat: box color change on mouseover

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let container = document.getElementById("container");
 document.createAttribute("class")
 
-
-
 for (i=0; i<256; i++)
 {
     let newDiv = document.createElement("div");
     container.appendChild(newDiv);
     newDiv.classList.add("box");
+}
+
+addEventListener("mouseover", funnyFn)
+
+function funnyFn(e)
+{
+    let r=randomNumber();
+    let g=randomNumber();
+    let b=randomNumber();
+    let color="rgba("+r+", "+g+", "+b+")"
+
+    if(e.target.classList=="box")
+    {
+        e.target.style.backgroundColor=color;
+    }
+    console.log(e.target)
+}
+
+function randomNumber()
+{
+    return Math.floor(Math.random()*255);
 }
 
 console.log(container);
