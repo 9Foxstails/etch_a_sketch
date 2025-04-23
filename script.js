5df9897594ed807c2b88c16fdfd0583a804ede06
9Foxstails <t-mozer@hotmail.com>
2025-04-23 10:38:57 +0300

    fix: size of grid squares, add header footer

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
 let container = document.getElementById("container");
-document.createAttribute("class")
+let btn = document.createElement("button");
+let body = document.getElementById("body");
 
-for (i=0; i<256; i++)
+document.body.insertBefore(btn, container);
+btn.classList.add("button");
+btn.innerHTML="Change grid size";
+
+let defaultDomState = document.body.innerHTML;
+const contWidth=960;
+
+//original grid
+let y=16;
+for (i=0; i<y*y; i++)
 {
     let newDiv = document.createElement("div");
     container.appendChild(newDiv);
     newDiv.classList.add("box");
+    //newDiv.style.opacity="0.1";
 }
+
+addEventListener("click", clackFn)
+function clackFn(e)
+{
+    let y=1;
+    if(e.target.classList=="button")
+    {
+        y=prompt("Enter a number between 2 and 100");
+        while(y>100)
+        {
+            y=prompt("Enter a number between 0 and 100");
+        }
+
+        while (container.firstChild)
+        {
+            container.removeChild(container.firstChild);
+        }
+
+        for (i=0; i<y*y; i++)
+        {
+            let newDiv = document.createElement("div");
+            container.appendChild(newDiv);
+            newDiv.classList.add("box");
+            let width=calcWidth(y);
+            let height=calcWidth(y);
+            newDiv.style.width=width+"px";
+            newDiv.style.height=height+"px";
+        }
+
+    }
+}
+
+
+
+
+
 
 addEventListener("mouseover", funnyFn)
 
@@ -20,8 +67,13 @@ function funnyFn(e)
     if(e.target.classList=="box")
     {
         e.target.style.backgroundColor=color;
+        if(+e.target.style.opacity<1)
+        {
+            x=+e.target.style.opacity;
+            x+=0.1;
+            e.target.style.opacity=x;
+        } 
     }
-    console.log(e.target)
 }
 
 function randomNumber()
@@ -29,4 +81,9 @@ function randomNumber()
     return Math.floor(Math.random()*255);
 }
 
-console.log(container);
+function calcWidth(num)
+{
+    //console.log(Math.ceil((1/num)*100));
+    return (contWidth/num);
+
+}
